1f77ef53b254035cf78a8af884244dbf620e03ec
Khush Patel <khush@lyzr.ai>
2024-02-29 16:32:19 +0300

    remove version

--- lyzr_rag/__init__.py
+++ lyzr_rag/__init__.py
@@ -2,10 +2,6 @@
 
 from pathlib import Path
 
-with open(Path(__file__).absolute().parents[0] / "VERSION") as _f:
-    __version__ = _f.read().strip()
-
-
 import logging
 from logging import NullHandler
 from typing import Callable, Optional
